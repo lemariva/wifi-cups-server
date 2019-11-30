8732dd763612f7fcdb29b5e2dcd76fd90c658674
Mauro Riva <lemariva@gmail.com>
2019-11-30 14:06:29 +0100

    - update base image - add gutenprint drivers (Canon printers) - update AP image

--- rpi-cup-server.sh
+++ rpi-cup-server.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#
+# Copyright [2019] [Mauro Riva - https://lemariva.com]
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
 echo "please enter the SSID name and press enter to continue:"
 read SSID
 echo "please enter the PASSWORD for the "$SSID" network and press enter to continue:"
@@ -15,21 +36,16 @@ echo "installing Docker..."
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
 
-# Docker 18.09.0CE is not working properly -> 18.06.1 is the alternative
-# apt-cache madison docker-ce
-echo "downgrading Docker..."
-sudo apt-get install docker-ce=18.06.1~ce~3-0~raspbian -y
-
 # pulling docker images
 echo "pulling Docker images..."
 sudo docker pull portainer/portainer
-sudo docker pull cjimti/iotwifi
+sudo docker pull lemariva/iotwifi
 sudo docker pull txn2/asws:armhf-1.2.3
-sudo docker pull lemariva/rpi-cups
+sudo docker pull lemariva/rpi-cups:latest
 
 # wificfg_configuration
 echo "downloading configurations and updating ssid/password"
-wget https://raw.githubusercontent.com/cjimti/iotwifi/master/cfg/wificfg.json
+wget https://raw.githubusercontent.com/lemariva/txwifi/master/cfg/wificfg.json
 sed -i 's/iot-wifi-cfg-3/'"$SSID"'/' wificfg.json
 sed -i 's/iotwifipass/'"$PASSWORD"'/' wificfg.json
 unset PASSWORD
@@ -40,7 +56,11 @@ git clone https://github.com/lemariva/wifi-cups-server.git
 mv wifi-cups-server/iot-website $(pwd)/www
 
 echo "starting to run the Containers..."
-echo "portainer runs on <ip-address>:9000. Please change the password!"
+echo "#########################################"
+echo "############## Security #################"
+echo "## portainer runs on <ip-address>:9000. #"
+echo "## Please change the admin password ##!!!"
+echo "#########################################"
 sudo docker volume create portainer_data
 sudo docker run -d -p 9000:9000 --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 echo "to configure the internet access go to  <ip-adress>:80"
@@ -59,4 +79,4 @@ sudo pkill wpa_supplicant
 sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
 
 echo "Iotwifi running on host"
-sudo docker run -d --restart unless-stopped --privileged --net host -v $(pwd)/wificfg.json:/cfg/wificfg.json  cjimti/iotwifi
+sudo docker run -d --restart unless-stopped --privileged --net host -v $(pwd)/wificfg.json:/cfg/wificfg.json lemariva/iotwifi
